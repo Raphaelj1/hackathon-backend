cde567842d607e13046d943f73f75e0dd1845208
Raphaelj <raphaeljames897@gmail.com>
2026-07-04 23:01:00 +0100

    setup better-auth and generate db schema for login/signup

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArcjetGuard, ArcjetModule, fixedWindow, shield } from '@arcjet/nest';
 import { APP_GUARD } from '@nestjs/core';
+import { AuthModule } from '@thallesp/nestjs-better-auth';
+import { auth } from './lib/auth/auth';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     ArcjetModule.forRoot({
       isGlobal: true,
       key: process.env.ARCJET_KEY!,
@@ -20,6 +24,7 @@ import { APP_GUARD } from '@nestjs/core';
         }),
       ],
     }),
+    AuthModule.forRoot({ auth }),
   ],
   controllers: [AppController],
   providers: [
